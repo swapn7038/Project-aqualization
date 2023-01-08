a9a8ac32803e3fab9d51108cb0d0099022d19373
Swapnil Shelke <Swapnilbabanshelke@gmail.com>
2023-01-08 16:03:19 +0530

    Updated

---
+++ assets/Js/script.js
@@ -0,0 +1,38 @@
+// const sectionEl = document.getElementsByClassName("section-one");
+
+// window.onload = function () {
+//   // Array of Images
+//   var backgroundImg = [
+//     // "./assets/images/img-2.jpg",
+//     "./assets/images/img-1.jpg",
+//     "./assets/images/img-3.jpg",
+//     // "./assets/images/img-4.jpg",
+//     "./assets/images/img-5.jpg",
+//     "./assets/images/img-7.jpg",
+//     "./assets/images/img-8.jpg",
+//   ];
+
+//   setInterval(changeImage, 500);
+//   function changeImage() {
+//     // var i = Math.floor(Math.random() * 8);
+//     var i = Math.floor(Math.random() * 4);
+
+//     document.body.style.backgroundImage = "url('" + backgroundImg[i] + "')";
+//   }
+// };
+
+//
+// function displayNextImage() {
+//     x = (x === images.length - 1) ? 0 : x + 1;
+//     document.getElementById("img").src = images[x];
+// }
+// function changeImage() {
+//     setInterval(displayNextImage, 5000);
+// }
+
+// var images = [], x = -1;
+// images[0] = "http://www.planwallpaper.com/static/images/desktop-year-of-the-tiger-images-wallpaper.jpg";
+// images[1] = "http://www.planwallpaper.com/static/images/background-gmail-google-images_FG2XwaO.jpg";
+// images[2] = "http://www.planwallpaper.com/static/images/beautiful-sunset-images-196063.jpg";
+
+// C:\Users\Swapnil\Desktop\Project+aqualization\assets\images\img-1.jpg
